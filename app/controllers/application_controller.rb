0b0deb3e4a5955c391550d039118e9153ba78d94
Elliot Mangini <elliot.mangini@gmail.com>
2022-10-03 13:05:42 -0600

    should be able to get current game now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # User Routes
-  # Grab User & In Progress
+  # Get User & In Progress
   get '/users/:username' do
     user = User.find_by(username: params[:username]).to_json
     in_progress = user.hunts.uniq
@@ -19,6 +19,9 @@ class ApplicationController < Sinatra::Base
     end
   end
 
-  # Visit Array Routes
+  # Get Current Game
+  get "/hunts/:user_id/:hunt_id/" do
+    User.find(params[:user_id]).visits.where(hunt_id: params[:id])
+  end
  
 end
