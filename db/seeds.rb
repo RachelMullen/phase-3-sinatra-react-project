d84262377e7e65479856a48274a2314e1ebb695c
Elliot Mangini <elliot.mangini@gmail.com>
2022-10-06 21:20:27 -0600

    seeds coming right together

--- db/seeds.rb
+++ db/seeds.rb
@@ -154,7 +154,7 @@ Visit.create(
     avoid: rand(2) == 1 ? true : false,
 )
 
-# daddy cham is killin shit
+puts "Daddy Cham is killin shit"
 
 User.create(username: "DaddyCham", bio: "Come to Papa", locale: "Around the Way", password: "123")
 
@@ -248,5 +248,102 @@ Comment.create(body: "That cream, that topmilk, PURE VANILLA... That's a 10!", u
 Comment.create(body: "Good walks, great talks", user_id: User.find_by(username: "DaddyCham").id, place_id: Place.find_by(name: "Cheesman Park").id)
 Comment.create(body: "Pricy but scrumptious", user_id: User.find_by(username: "DaddyCham").id, place_id: Place.find_by(name: "Torchy's Tacos").id)
 Comment.create(body: "Zoo's be evil me and my homies hate the zoo", user_id: User.find_by(username: "DaddyCham").id, place_id: Place.find_by(name: "Denver Zoo").id)
+
+puts "Rachel is seeding!"
+
+User.create(username: "Mullet", bio: "Popcorn is ah acceptable snack and dinner.", locale: "Barnum, Denver", password: "123")
+
+Hunt.create(title: "Dive Bars", user_id: User.find_by(username: "Mullet"), public: true)
+Hunt.create(title: "Nerd Stuff", user_id: User.find_by(username: "Mullet"), public: true)
+
+
+Place.create(name: "Don's Club Tavern", description: "Unassuming veteran with vinyl booths & wood paneling featuring cocktails, pool & an outdoor patio.", latitude: 39.72594870943603, longitude: -104.97816344433357)
+Place.create(name: "Candlelight Tavern", description: "A dark, chill tradition for booze bargains, burgers, sports on TV & games from pool to shuffleboard.", latitude: 39.710226257311305, longitude: -104.98042022268027)
+Place.create(name: "Lakeview Lounge", description: "Vintage, cash-only dive bar offering pool & foosball tables & a patio overlooking Sloan's Lake.", latitude: 39.75103821469635, latitude: -105.05363051549632)
+Place.create(name: "Viking", description: "Biker bar on West Colfax.", latitude: 39.74091602393617, longitude: -105.04958577517503)
+Place.create(name: "Wings Over the Rockies Air & Space Museum", description: "Wartime aircraft, WWII uniform collection & aviation art on the former site of Lowry Air Force Base.", latitude: 39.72096494977194, latitude: -104.89550750200648)
+Place.create(name: "Denver Public Library: Central Library", description: "Take a look, it's in a book!", latitude: 39.742172513731724, longitude: -104.9860303515646)
+Place.create(name: "History Colorado Center", description: "Interactive museum showcasing state-history exhibits, lectures & more in an eco-friendly building.", latitude: 39.73740196872607, longitude: -104.9881886330873)
+Place.create(name: "Denver Museum of Nature & Science", description: "Take a look, it's in a book!", latitude: 39.74768281167681, longitude: -104.94289363269709)
+
+Visit.create(
+    user_id: User.find_by(username: "Mullet"),
+    place_id: Place.find_by(name: "Don's Club Tavern").id,
+    hunt_id: Hunt.find_by(title: "Dive Bars").id,
+    complete: false,
+    wishlist: true,
+    favorite: false,
+    avoid: false,
+)
+Visit.create(
+    user_id: User.find_by(username: "Mullet"),
+    place_id: Place.find_by(name: "Candlelight Tavern").id,
+    hunt_id: Hunt.find_by(title: "Dive Bars").id,
+    complete: false,
+    wishlist: false,
+    favorite: false,
+    avoid: true,
+)
+Visit.create(
+    user_id: User.find_by(username: "Mullet"),
+    place_id: Place.find_by(name: "Lakeview Lounge").id,
+    hunt_id: Hunt.find_by(title: "Dive Bars").id,
+    complete: false,
+    wishlist: false,
+    favorite: true,
+    avoid: false,
+)
+Visit.create(
+    user_id: User.find_by(username: "Mullet"),
+    place_id: Place.find_by(name: "Viking").id,
+    hunt_id: Hunt.find_by(title: "Dive Bars").id,
+    complete: false,
+    wishlist: false,
+    favorite: true,
+    avoid: false,
+)
+Visit.create(
+    user_id: User.find_by(username: "Mullet"),
+    place_id: Place.find_by(name: "Denver Public Library: Central Library").id,
+    hunt_id: Hunt.find_by(title: "Nerd Stuff").id,
+    complete: false,
+    wishlist: false,
+    favorite: false,
+    avoid: true,
+)
+Visit.create(
+    user_id: User.find_by(username: "Mullet"),
+    place_id: Place.find_by(name: "History Colorado Center").id,
+    hunt_id: Hunt.find_by(title: "Nerd Stuff").id,
+    complete: false,
+    wishlist: true,
+    favorite: false,
+    avoid: false,
+)
+Visit.create(
+    user_id: User.find_by(username: "Mullet"),
+    place_id: Place.find_by(name: "Wings Over the Rockies Air & Space Museum").id,
+    hunt_id: Hunt.find_by(title: "Nerd Stuff").id,
+    complete: false,
+    wishlist: false,
+    favorite: true,
+    avoid: false,
+)
+Visit.create(
+    user_id: User.find_by(username: "Mullet"),
+    place_id: Place.find_by(name: "Denver Museum of Nature & Science").id,
+    hunt_id: Hunt.find_by(title: "Nerd Stuff").id,
+    complete: false,
+    wishlist: false,
+    favorite: true,
+    avoid: false,
+)
+
+Comment.create(body: "You will absolutely leave with stories from this place.", user_id: User.find_by(username: "Mullet").id, place_id: Place.find_by(name: "Candlelight Tavern").id)
+Comment.create(body: "This place has a table from the G8 summit in the 1990s!", user_id: User.find_by(username: "Mullet").id, place_id: Place.find_by(name: "Denver Public Library: Central Library").id)
+Comment.create(body: "Who doesn't love NASA? Check it out!", user_id: User.find_by(username: "Mullet").id, place_id: Place.find_by(name: "Wings Over the Rockies Air & Space Museum").id)
+Comment.create(body: "They just don't make bars like this anymore, you know?", user_id: User.find_by(username: "Mullet").id, place_id: Place.find_by(name: "Lakeview Lounge").id)
+Comment.create(body: "It's not for me, but locals love it.", user_id: User.find_by(username: "Mullet").id, place_id: Place.find_by(name: "Viking").id)
+Comment.create(body: "This is an amazing step into Colorado's past, present, and future!", user_id: User.find_by(username: "Mullet").id, place_id: Place.find_by(name: "History Colorado Center").id)
 
 puts "✅ Done seeding!"
